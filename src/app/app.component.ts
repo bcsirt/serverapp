6215ec35161f55cae06955922424037688ffd291
Laurent LOUIS-THERESE <4466339+bcsirt@users.noreply.github.com>
2022-02-11 16:02:35 +0100

    added dynamic elements for the table and improved error handling on the front side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, Observable, of, startWith, pipe } from 'rxjs';
+import { catchError, Observable, of, startWith, pipe, BehaviorSubject } from 'rxjs';
 import { map, filter, tap } from 'rxjs/operators'
 import { DataState } from './enum/data-state-enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -13,6 +14,12 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  filterStatus$ = this.filterSubject.asObservable();
+
+
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
